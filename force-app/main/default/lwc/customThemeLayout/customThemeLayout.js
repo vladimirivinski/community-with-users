43cd12f4e641f8885df014b67b051de332588a1f
Vladimir <vladimir.ivinski89@gmail.com>
2022-11-16 23:35:51 +0300

    added  lwr custom layout theme

---
+++ force-app/main/default/lwc/customThemeLayout/customThemeLayout.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+/**
+* @slot Header-Region
+* @slot Content-Region
+* @slot Footer-Region
+*/
+
+export default class CustomThemeLayout extends LightningElement {}
